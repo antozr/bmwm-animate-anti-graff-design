40acb5265653f627c5e77191fd906c285832ab64
anto__dev <antonidwm@gmail.com>
2024-01-10 00:24:54 +0100

    ADD FIrst section

--- bmwm-webanim/src/page/landing.jsx
+++ bmwm-webanim/src/page/landing.jsx
@@ -1,19 +1,33 @@
 //import React from 'react'
 //import NavBar from '../routes/NavBar'
-import Header from '../components/Header'
+import Header from "../components/Header";
 
 const Landing = () => {
   return (
     <>
-    <Header />
-    <div className="sect sect--1">
-      landing
-    </div>
-    <div className="sect" id="naissance">
-      naissance
-    </div>
-    </>
-  )
-}
+      <Header />
+      <div className="sect sect--1">
+        <h1 className="sect__title sect__title--decorPrincipale">L’histoire <br></br> d’une marque</h1>
+        <p className="sect__title sect__title--decorHB">
+          Mythique
+        </p>
+      </div>
+      <div className="interSection__bmwBandeG">
+        <div className="interSection__bande interSection__bande--bc">
 
-export default Landing
+        </div>
+        <div className="interSection__bande interSection__bande--bf">
+
+        </div>
+        <div className="interSection__bande interSection__bande--rg">
+
+        </div>
+      </div>
+      <div className="sect" id="naissance">
+        naissance
+      </div>
+    </>
+  );
+};
+
+export default Landing;
