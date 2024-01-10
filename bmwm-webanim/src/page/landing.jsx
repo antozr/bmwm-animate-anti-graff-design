ba4640767a267fcf438f1d3887c9531cdda7ae5a
anto__dev <antonidwm@gmail.com>
2024-01-10 02:25:24 +0100

    ADD content second section

--- bmwm-webanim/src/page/landing.jsx
+++ bmwm-webanim/src/page/landing.jsx
@@ -7,25 +7,49 @@ const Landing = () => {
     <>
       <Header />
       <div className="sect sect--1">
-        <h1 className="sect__title sect__title--decorPrincipale">L’histoire <br></br> d’une marque</h1>
-        <p className="sect__title sect__title--decorHB">
-          Mythique
-        </p>
+        <h1 className="sect__title sect__title--decorPrincipale">
+          L’histoire <br></br> d’une marque
+        </h1>
+        <p className="sect__title sect__title--decorHB">Mythique</p>
       </div>
       <div className="interSection__bmwBandeG">
-        <div className="interSection__bande interSection__bande--bc">
-
-        </div>
-        <div className="interSection__bande interSection__bande--bf">
-
-        </div>
-        <div className="interSection__bande interSection__bande--rg">
-
-        </div>
+        <div className="interSection__bande interSection__bande--bc"></div>
+        <div className="interSection__bande interSection__bande--bf"></div>
+        <div className="interSection__bande interSection__bande--rg"></div>
       </div>
-      <div className="sect" id="naissance">
-        naissance
-      </div>
+      <section className="sect sect--2" id="naissance">
+        <div className="sect__blocText sect__blocText--decor">
+          <p className="sect__title sect__title--blue">Découvrez</p>
+          <p className="sect__title sect__title--blue sect__title--decorCentrale">
+            la naissance des véhicules de
+          </p>
+          <p className="sect__title sect__title--red">legende</p>
+        </div>
+        <div className="sect__blocImg">
+          <div className="blocImg blockImg__smallH">
+
+            <img
+              src="assets/img/bmwSite/bmw-02-old-1.webp"
+              alt="This is an old bmw 02."
+              className="blocImg__img"
+            />
+          </div>
+          <div className="blocImg blockImg__smallH">
+            {/* <img
+              src="assets/img/bmwSite/bmw-bw-1.webp"
+              alt="This is an old bmw."
+              className="blocImg__img"
+            /> */}
+          </div>
+          <div className="blocImg blockImg__smallH">
+            <img
+              src="assets/img/bmwSite/bmw-right-1.webp"
+              alt="This is an old bmw."
+              className="blocImg__img"
+            />
+          </div>
+        </div>
+      </section>
     </>
   );
 };
